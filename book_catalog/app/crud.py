e3aa5255e9d00a24d0bf08649360556fe676d2e7
Vikram <129699513+vikram-42@users.noreply.github.com>
2024-09-09 19:36:43 +1000

    Update

--- book_catalog/app/crud.py
+++ book_catalog/app/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+import models, schemas
 
 def get_book(db: Session, book_id: int):
     return db.query(models.Book).filter(models.Book.id == book_id).first()
