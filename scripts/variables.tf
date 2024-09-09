4e67986b8b9a86fdb207152a6f75783c4e0cb212
Vikram <129699513+vikram-42@users.noreply.github.com>
2024-09-09 19:43:12 +1000

    Add files via upload

---
+++ scripts/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  default = "part4"            
+}
+
+variable "location" {
+  default = "australiasoutheast"  
+}
+
+variable "kubernetes_version" {    
+  default = "1.30.0"           
+}
